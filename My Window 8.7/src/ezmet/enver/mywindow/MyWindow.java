b658a47c94ae92e61bb240301fc20844cada514b
EnverJ <Enlem1987@gmail.com>
2022-04-30 22:26:04 -0600

    repeat

--- My Window 8.7/src/ezmet/enver/mywindow/MyWindow.java
+++ My Window 8.7/src/ezmet/enver/mywindow/MyWindow.java
@@ -3,42 +3,66 @@ package ezmet.enver.mywindow;
 import com.godtsoft.diyjava.DIYWindow;
 
 public class MyWindow extends DIYWindow {
-	
-	protected int promtForInt(String prompt){
-		
-		int i=0;
+
+	protected int promtForInt(String prompt) {
+
+		int i = 0;
 		print(prompt);
-		String s=input();
-		try{
-			i=Integer.parseInt(s);
-		}
-		catch(NumberFormatException e){
+		String s = input();
+		try {
+			i = Integer.parseInt(s);
+		} catch (NumberFormatException e) {
 			print(s + " Is not a valid number. try again.");
-			i=promtForInt(prompt);
+			i = promtForInt(prompt);
 		}
-		
+
 		return i;
-		
-		
+
 	}
-	protected String promtFor(String prompt){
+
+	protected String promtFor(String prompt) {
 		print(prompt);
-		String s=input();
+		String s = input();
 		return s;
 	}
-	
-	protected double promtForDouble(String promt){
-		double d=0;
+
+	protected double promtForDouble(String promt) {
+		double d = 0;
 		print(promt);
-		String s=input();
-		try{
-			d=Double.parseDouble(s);
-		}
-		catch(NumberFormatException e){
+		String s = input();
+		try {
+			d = Double.parseDouble(s);
+		} catch (NumberFormatException e) {
 			print(s + " is not a valid number. Try again.");
-			
+
 		}
 		return d;
+	}
+
+	protected boolean prometForYesNo(String promt) {
+		boolean yes = true;
+		boolean valid = false;
+		while (!valid) {
+			String YorN = promtFor(promt);
+			switch (YorN) {
+			case "Y":
+			case "y":
+				yes = true;
+				valid = true;
+				break;
+			case "N":
+			case "n":
+				yes = false;
+				valid = true;
+				break;
+			default:
+				print("Enter Y for yes, or N for no. ");
+				valid = false;
+
+			}
+
+		}
+		return yes;
 	}
 
 }
