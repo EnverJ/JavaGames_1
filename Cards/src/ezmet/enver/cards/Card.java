843319974124dba4f76fcb7407daf0ba5efcded2
EnverJ <Enlem1987@gmail.com>
2022-05-12 21:56:40 -0600

    crazy eights

---
+++ Cards/src/ezmet/enver/cards/Card.java
@@ -0,0 +1,12 @@
+package ezmet.enver.cards;
+
+public class Card {
+	private char rank='8';
+	private char suit='H';
+	public String toString(){
+		String rs=""+rank+suit;
+		return rs;
+		
+	}
+
+}
