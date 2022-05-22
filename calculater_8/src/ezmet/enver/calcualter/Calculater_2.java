48134dee08e51cec811522374fee14702c1d44eb
EnverJ <Enlem1987@gmail.com>
2022-05-21 21:45:30 -0600

    Saturday

--- calculater_8/src/ezmet/enver/calcualter/Calculater_2.java
+++ calculater_8/src/ezmet/enver/calcualter/Calculater_2.java
@@ -6,45 +6,41 @@ public class Calculater_2 extends MyWindow {
 
 	public Calculater_2() {
 		setFontSize(25);
-		String s="Y";
-		while(s.startsWith("Y")||s.startsWith("y")){
-		double a=promtForDouble("Enter a number");
-		String operation=promtFor("Enter +,-,* or /");
-		double b=promtForDouble("Enter another number");
-		double c=0;
-		switch(operation){
-		case"-":
-			c=(a-b);
-			print(a+"-"+b+"="+c);
-			break;
-		case"+":
-			c=(a+b);
-			print(a+"+"+b+"="+c);
-			break;
-		case"*":
-			c=a*b;
-			print(a+"*"+b+"="+c);
-			break;
-		case"/":
-			try{
-				c=a/b;
-				print(a+"/"+b+"="+c);
+		String s = "Y";
+		while (s.startsWith("Y") || s.startsWith("y")) {
+			double a = promtForDouble("Enter a number");
+			String operation = promtFor("Enter +,-,* or /");
+			double b = promtForDouble("Enter another number");
+			double c = 0;
+			switch (operation) {
+			case "-":
+				c = (a - b);
+				print(a + "-" + b + "=" + c);
+				break;
+			case "+":
+				c = (a + b);
+				print(a + "+" + b + "=" + c);
+				break;
+			case "*":
+				c = a * b;
+				print(a + "*" + b + "=" + c);
+				break;
+			case "/":
+				try {
+					c = a / b;
+					print(a + "/" + b + "=" + c);
+				} catch (ArithmeticException e) {
+					print("Division by zero is not allowed");
+
+				}
+				break;
+			default:
+				print("An invalid operation was entered.");
+
 			}
-			catch(ArithmeticException e){
-				print("Division by zero is not allowed");
-				
-			}
-			break;
-		default:
-			print("An invalid operation was entered.");
-			
+			s = promtFor("Do you wan to do another?");
 		}
-		s=promtFor("Do you wan to do another?");
-		
-			
-		
-		}
-		
+
 	}
 	
 
